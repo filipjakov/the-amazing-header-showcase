030c3f6c52ff5085bb3a6c6adc5388b6ca1639c6
Filip Jakov Bulic <filip.jakov.bulic@infinum.hr>
2021-03-11 21:51:32 +0100

    Final product

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # the-amazing-header-showcase
-Walkthrough of creating a header component for JS Standup 
+
+Walkthrough of creating a header component for JS Standup
+
+Progressive enhancment approach
+
+1. Plain old HTML
+
+- talk about aria roles
+- inline svg width
+
+2. CSS
+
+3. JS
--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,42 @@
-console.log("Hello");
+// Listen to hash changes
+const onHashChange = () => {
+  const isOpen = document.location.hash === "#nav-open";
+
+  document.body.classList.toggle("lock", isOpen);
+
+  isOpen
+    ? document.querySelector('a[href="#_"]').focus()
+    : document.querySelector('a[href="#nav-open"]').focus();
+};
+
+window.addEventListener("hashchange", onHashChange, {
+  passive: true,
+});
+
+// Why calling it initally?
+onHashChange();
+
+// Listen to media changes
+const onScreenMatch = ({ matches }) =>
+  document.body.classList.toggle(
+    "lock",
+    document.location.hash === "#nav-open" && matches
+  );
+
+const mediaQueryList = window.matchMedia("(max-width: 48em)");
+
+mediaQueryList.addListener(onScreenMatch);
+
+// Why calling it initally?
+onScreenMatch({ matches: mediaQueryList.matches });
+
+// IO detects when element sticks
+const header = document.querySelector("header");
+const observer = new IntersectionObserver(
+  ([e]) => e.target.classList.toggle("is-pinned", e.intersectionRatio < 1),
+  { threshold: [1] }
+);
+
+observer.observe(header);
+
+// TODO: focus traps/inert -> covered by Kiki
